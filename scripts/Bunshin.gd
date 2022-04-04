4d2d4053e1093ec999dc72a958014d705f4d8ba3
Fabrice Jeannet <fabricejeannet@fabrice.coolworking.fr>
2022-04-04 15:30:07 +0200

    Still working on Bunshin

--- scripts/Bunshin.gd
+++ scripts/Bunshin.gd
@@ -1,21 +1,37 @@
 extends Spell
 class_name Bunshin
 
-export var duration:int 
 var character:KinematicBody2D
+var smoke:KinematicBody2D
+var bunshin:KinematicBody2D
+var animation_player:AnimationPlayer
+var position_offset:Vector2
+
+export var duration:int 
+export var position_offset_coeff:Vector2
 
 func cast(_character:KinematicBody2D, mouse_pointer:Vector2) -> void:
 	
 	character = _character
-	var bunshin = $KinematicBody2D
-	bunshin.position = character.sight.global_position
+	bunshin = $KinematicBody2D
+	position_offset =  (character.sight.global_position - character.global_position) * position_offset_coeff
 
-	
-	var animation_player = $KinematicBody2D/APSmoke
-	
+	smoke = $SmokeKB2D
+	smoke.position = bunshin.position
+	animation_player = $SmokeKB2D/APSmoke
 	animation_player.play("SmokeBomb")
-	yield(get_tree().create_timer(duration), "timeout")
+	if duration > 0:
+		yield(get_tree().create_timer(duration), "timeout")
+		destroy()
+	
+
+func destroy() -> void :
+	smoke.position = bunshin.position
+	bunshin.visible = false
 	animation_player.play("SmokeBomb")
 	yield(animation_player,"animation_finished")
-	bunshin.visible = false
+
 	call_deferred("free")
+
+func _process(delta):
+	bunshin.position = character.position + position_offset
